933390f58bf16daf4b3db64965d303c16cbf10d6
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-24 00:14:27 +0530

    binary to decimal recurion

--- b2dProblem3.c
+++ b2dProblem3.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+
+struct stack
+{
+  int data[20];
+  int top;
+}st;
 
 int b2d(int bin)
 {
-  int sum = 0;
-  if(bin == 1)
-    return 1;
-  if(bin == 0)
-    return 0;
+  if(bin == 1 || bin == 0)
+    st.data[++st.top] = bin;
   else
   {
-    sum = b2d(bin % 10) + b2d(bin / 10);
+    b2d(bin/10);
+    st.data[++st.top] = bin%10;
   }
-  return sum;
 }
 
 int main()
 {
+  st.top = -1;
   int bin;
+  int sum = 0;
   printf("Enter Binary no: ");
   scanf("%d",&bin);
-  printf("Decimal form of %d is: %d\n",bin, b2d(bin));
+  for(int i = 0 ; i <= st.top ; i++)
+  {
+    sum = sum + st.data[i]*pow(2,i);
+  }
+  printf("Decimal form of %d is: %d\n",bin,sum);
   return 0;
 }
