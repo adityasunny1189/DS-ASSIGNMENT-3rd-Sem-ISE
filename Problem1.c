9c2c42fbae8f53e3c1e031db62cd1671412f2f65
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-24 00:13:37 +0530

    file encryption

--- Problem1.c
+++ Problem1.c
@@ -4,29 +4,37 @@
 
 int main()
 {
-  FILE *fp1;
-  char name[20] ,passw[20] , passwt[20] = "Aditya98716";
-  printf("Enter the file name to open: ");
-  scanf("%s",name);
-  fp1 = fopen(name , "r");
+  printf("File Encryption Program\n");
+  FILE *fp1 , *fp2;
+  char file1[20] , file2[20];
+  printf("Enter the name of the file to read: ");
+  scanf("%s",file1);
+  fp1 = fopen(file1, "r");
   if(fp1 == NULL)
   {
-    printf("Invalid file name or destination\n");
+    printf("File does not exist!\n");
     exit(1);
   }
-  else
+  printf("Enter the name of the output file: ");
+  scanf("%s",file2);
+  fp2 = fopen(file2 , "w");
+  if(fp2 == NULL)
   {
-    printf("Enter Password: ");
-    scanf("%s",passw);
-    if(!strcmp(passwt , passw))
-    {
-      printf("Opening File\n");
-    }
-    else
-    {
-      printf("Invalid Password\n");
-      exit(1);
-    }
+    printf("Invalid Destination\n");
+    fclose(fp1);
+    exit(1);
   }
+  char ch;
+  while(1)
+  {
+    ch = fgetc(fp1);
+    if(ch == EOF)
+      break;
+    ch = ch + 1;
+    fputc(ch, fp2);
+  }
+  fclose(fp1);
+  fclose(fp2);
+  printf("File Sucessfully Encrypted\n");
   return 0;
 }
